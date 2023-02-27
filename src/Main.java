0d3a8c21d58382cbe8e3736189bd887f2994dc3b
Jeferson <jefersonlopeseugenio11@gmail.com>
2023-02-27 16:10:31 -0300

    Tirei parametro parte limite e modifica valor saque

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ContaBancaria conta1 = new ContaBancaria("12345-6", "Jeferson", 1000, 200);
+        ContaBancaria conta1 = new ContaBancaria("12345-6", "Jeferson", 1000);
 
         conta1.deposito(500);
-        conta1.saque(1600);
+        conta1.saque(200);
         conta1.imprimirInformacoes();
     }
 }
